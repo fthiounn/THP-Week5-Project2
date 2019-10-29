e7a10d72d43c1981f762be9adda50c3d77f2b6b0
fthiounn <fthiounn@gmail.com>
2019-10-29 09:06:42 +0100

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module THPWeek5Project2
+module THPWeek5Project1
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
