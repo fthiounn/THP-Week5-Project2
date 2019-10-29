92fe0a3f6cb4dfda58fbedb41bf82c1cf2fc7430
fthiounn <fthiounn@gmail.com>
2019-10-29 09:15:13 +0100

    fix likes in db (linking it to a user

--- db/migrate/20191028083951_create_likes.rb
+++ db/migrate/20191028083951_create_likes.rb
@@ -1,6 +1,7 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
+      t.belongs_to :user, index: true
       t.belongs_to :gossip, index: true
       t.belongs_to :comment, index: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -53,12 +53,14 @@ ActiveRecord::Schema.define(version: 2019_10_28_084021) do
   end
 
   create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
     t.bigint "gossip_id"
     t.bigint "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["comment_id"], name: "index_likes_on_comment_id"
     t.index ["gossip_id"], name: "index_likes_on_gossip_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "private_messages", force: :cascade do |t|
